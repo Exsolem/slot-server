335bbaf3b37d0fd87576f5c3e2e7917379816be1
= <=>
2022-06-16 16:44:21 +0300

    tutorial applied

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const https = require('https');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-const httpsServer = https.createServer(app)
-.listen(4000, ()=>{
-  console.log('server is runing at port 4000')
-});
-
-app.use(cors());
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => mySend(res))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+//app.use(cors());
 
 const getArr = (count) => {
   const arr = []
@@ -46,9 +46,9 @@ const getCombinations = (arr) => {
 const mySend = (res) => {
   res.send(JSON.stringify(getArr(25)))
 }
-app.get('/', (req, res) => {    
-    mySend(res);
-})
+// app.get('/', (req, res) => {    
+//     mySend(res);
+// })
 
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`)
